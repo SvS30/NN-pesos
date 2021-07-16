2c231133c21596324ad8f201be97eaa119a4b1da
SalimVazquez <181086@ids.upchiapas.edu.mx>
2021-07-15 20:28:44 -0500

    Split dataframe for training and testing

--- NN.R
+++ NN.R
@@ -4,3 +4,9 @@ medidas <- read.table(file.choose(), header=TRUE)
 # convert to dataframe
 medFrame <- as.data.frame(medidas)
 head(medFrame)
+
+# split dataframe for train and test
+# 348 registers for training
+# 116 registers for testing
+trainset <- medFrame[1:348,]
+testset <- medFrame[349:463,]
