e8788b4f4dcbf75e8b0716dfb1cec3ab7bd34867
SalimVazquez <181086@ids.upchiapas.edu.mx>
2021-07-15 21:08:36 -0500

    Predictions without rounded

--- NN.R
+++ NN.R
@@ -3,6 +3,7 @@ medidas <- read.table(file.choose(), header=TRUE)
 
 # convert to dataframe
 medFrame <- as.data.frame(medidas)
+# print the 6 first records
 head(medFrame)
 
 # split dataframe for train and test
@@ -18,6 +19,7 @@ testset <- medFrame[349:463,]
 # without hidden layer
 # act function does not apply
 # error function as sopping criteria = 0.01
+# this NN is for training
 library(neuralnet)
 nn <- neuralnet(`expected` ~ `nave` + `mes` + `alimentos`, data=trainset, hidden=0, linear.output=FALSE, threshold=0.01)
 
@@ -27,3 +29,14 @@ nn$result.matrix
 
 # display neural network as graph
 plot(nn)
+
+# process for testing
+# split columns from testset DataFrame
+temp_test <- subset(testset, select = c("nave", "mes", "alimentos"))
+head(temp_test)
+
+# collect NN and data for testing in a new DataFrame
+nn.results <- compute(nn, temp_test)
+# transform the input data into a standardized format
+results <- data.frame(actual = testset$expected, prediction = nn.results$net.result)
+head(results)
