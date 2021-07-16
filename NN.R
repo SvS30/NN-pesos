86e3f4580a438a1df3136cde33966f5f23e95500
SalimVazquez <181086@ids.upchiapas.edu.mx>
2021-07-15 20:44:30 -0500

    adding nn and showing results

--- NN.R
+++ NN.R
@@ -10,3 +10,20 @@ head(medFrame)
 # 116 registers for testing
 trainset <- medFrame[1:348,]
 testset <- medFrame[349:463,]
+
+# library for neural networks
+# https://www.rdocumentation.org/packages/neuralnet/versions/1.44.2/topics/neuralnet
+# output = expected
+# inputs (nave, mes, alimentos)
+# without hidden layer
+# act function does not apply
+# error function as sopping criteria = 0.01
+library(neuralnet)
+nn <- neuralnet(`expected` ~ `nave` + `mes` + `alimentos`, data=trainset, hidden=0, linear.output=FALSE, threshold=0.01)
+
+# Printing the results matrix
+# with information such as the error, steps, and expected values
+nn$result.matrix
+
+# display neural network as graph
+plot(nn)
